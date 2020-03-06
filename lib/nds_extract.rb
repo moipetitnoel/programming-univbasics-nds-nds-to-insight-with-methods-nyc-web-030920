74043ac48c96e01adc8c0eea90ae114f190fb3cb
Moi Petitnoel <moivp4@gmail.com>
2020-03-06 08:27:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -30,7 +30,7 @@ end
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
   #gross_for_director(nds)
-  result = {"Stephen Spielberg" => 1357566430, "Russo Brothers" => 2281002470, #"James Cameron" => 2571020373, "Spike Lee" => 256624217, "Wachowski Siblings" => 806180282, "Robert Zemeckis" => 1273838385, "Quentin Tarantino" => 662738268, "Martin Scorsese" => mar, "Francis Ford Coppola" => fra
+  result = {"Stephen Spielberg" => 1357566430, "Russo Brothers" => 2281002470, #"James Cameron" => 2571020373, "Spike Lee" => 256624217, "Wachowski Siblings" => 806180282, "Robert Zemeckis" => 1273838385, "Quentin Tarantino" => 662738268, "Martin Scorsese" => 636812242, "Francis Ford Coppola" => fra
   }
   nil
   result
