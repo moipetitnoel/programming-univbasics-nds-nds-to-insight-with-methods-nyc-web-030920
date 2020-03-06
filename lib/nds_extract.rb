20037c16cb4fc484f4efb7a14dd24932780b1834
Moi Petitnoel <moivp4@gmail.com>
2020-03-06 08:20:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,7 +29,7 @@ end
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
-  gross_for_director(nds)
+  #gross_for_director(nds)
   result = {"Stephen Spielberg" => gross_for_director(director_data)}
   
   nil
